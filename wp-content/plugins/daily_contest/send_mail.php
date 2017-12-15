817e8c8ea279617622b649d4e66d10499029ed0b
shamelessApathy <shamelessApathy>
2017-12-15 05:04:46 +0000

    live site now, need to fix email setrtings it will only emailto my accounts email

--- wp-content/plugins/daily_contest/send_mail.php
+++ wp-content/plugins/daily_contest/send_mail.php
@@ -4,17 +4,21 @@
 $require = $_SERVER['DOCUMENT_ROOT'].'/wp-load.php';
 require($require);
 
+$email = $_POST['email'];
 
 
 
-$to = "bcm811@gmail.com";
-$subject = "Test Email from wordpress plugin";
-$message = "This is a test message from a wordpress contest plugin";
+
+$to = $email;
+$subject = "You won the bong-a-day giveaway!";
+$message = "This message has been sent to you on behalf of girlsgonehigh.com, your entry into the bong-a-day giveaway has won!!!! Your new bong should be shipping out anyday now!";
 $headers = '';
 $attachments = array();
 $happen = wp_mail($to,$subject,$message,$headers,$attachments);
 
 
 var_dump($happen);
+
+var_dump($email);
 // wp_mail( string|array $to, string $subject, string $message, string|array $headers = '', string|array $attachments = array() )
 ?>
--- wp-content/plugins/daily_contest/shortcode/front.contest_main.php
+++ wp-content/plugins/daily_contest/shortcode/front.contest_main.php
@@ -7,18 +7,27 @@ $user_id = get_current_user_id();
 ?>
 <div id='contest-form-holder'>
 <?php if ($logged_in):?>
-<h3 style='text-align:center;'>Daily Contest</h3>
+<h3 style='text-align:center; color:#ccc;'>Bong-a-Day Giveaway!</h3>
 <?php if($test):?>
 	<?php echo "<h3>Thanks for playing come back tomorrow!</h3>";?>
 <?php else:?>
-<div id='dc-daily-contest-button-holder'>
-<button id='dc-daily-contest-button' data-user-id="<?php echo $user_id;?>">Click to Enter!!</button>
+<div id='dc-logo-container'>
+	<img src="http://girlsgonehigh.com/wp-content/uploads/2017/12/ggh_logo_sm.jpg"/>
+	<div id='dc-daily-contest-button-holder'>
+		<button id='dc-daily-contest-button' data-user-id="<?php echo $user_id;?>">Click to Enter!!</button>
+
 </div>
+</div>
+
 <?php endif;?>
 <?php else:?>
-<div id='dc-daily-contest-button-holder'>
+<div id='dc-logo-container'>
+	<img src="http://girlsgonehigh.com/wp-content/uploads/2017/12/ggh_logo_sm.jpg"/>
+	<div id='dc-daily-contest-button-holder'>
 <button id='dc-login-button'>Login to Enter Contest</button>
 </div>
+</div>
+
 <?php endif;?>
 </div>
 
